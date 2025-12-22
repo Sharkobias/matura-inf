74d9bd4ebb1907c32ccf1bcde568c492ee1df493
Tobias Fajks <toby.fajks@gmail.com>
2025-12-22 16:10:33 +0100

    1.1

---
+++ 2025czerwiec.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+int skroc(int x) 
+{
+    int y = x % 10;
+    if (x == y)
+        return 0;
+    else
+        return (x - y)/10;
+}
+int dopisz(int x) 
+{
+    return x * 10;
+}
+int ost(int x) 
+{
+    return x % 10;
+}
+int f(int a, int b) 
+{
+    if (b == 0)
+        return 0;
+    else 
+    {
+        int k = ost(b);
+        int w = f(a, skroc(b));
+        w = dopisz(w);
+        while (k > 0) 
+        {
+            w = w + a;
+            k = k - 1;
+        }
+        return w;
+    }
+}
+int main()
+{
+    cout << f(42,2) << endl;
+    cout << f(4, 125) << endl;
+    cout << f(103, 104) << endl;
+}
