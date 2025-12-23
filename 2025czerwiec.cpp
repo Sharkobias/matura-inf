2538c3afb48ee3ae5806b9c228025744903a2a78
Tobias Fajks <toby.fajks@gmail.com>
2025-12-23 12:31:21 +0100

    2.1

--- 2025czerwiec.cpp
+++ 2025czerwiec.cpp
@@ -1,44 +1,27 @@
 ﻿#include <iostream>
 using namespace std;
-int c = 0;
-int skroc(int x) 
+int ten(int x) 
 {
-    int y = x % 10;
-    if (x == y)
-        return 0;
-    else
-        return (x - y)/10;
-}
-int dopisz(int x) 
-{
-    return x * 10;
-}
-int ost(int x) 
-{
-    return x % 10;
-}
-int f(int a, int b) 
-{
-    if (b == 0)
-        return 0;
-    else 
-    {
-        int k = ost(b);
-        int w = f(a, skroc(b));
-        w = dopisz(w);
-        while (k > 0) 
-        {
-            w = w + a;
-            c++;
-            k = k - 1;
-        }
-        return w;
-    }
+	int res = 10;
+	for(int i = x; i!=1; i--)
+	{
+		res=res * 10;
+	}
+	return res;
 }
 int main()
 {
-    
-    cout << f(2024, 1000) << " " << c << endl;
-    c = 0;
-    cout << f(2024, 1234) << " " << c << endl;
+	int k = 123456;
+	int a = 0;
+	int b = 0;
+	int leng = 0;
+	int cop = k;
+	while (cop != 0) 
+	{
+		cop = cop / 10;
+		leng++;
+	} 
+	b = k % ten(leng / 2);
+	a = (k / ten(leng/2));
+	cout << a << " " << b << endl;
 }
