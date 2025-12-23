4b8aaa0971bf0014380cf93e39d30b3616328c92
Tobias Fajks <toby.fajks@gmail.com>
2025-12-23 14:20:57 +0100

    3.1

--- 2025czerwiec.cpp
+++ 2025czerwiec.cpp
@@ -2,49 +2,24 @@
 #include <fstream>
 #include <string>
 using namespace std;
-int ten(int x) 
-{
-	int res = 10;
-	for(int i = x; i!=1; i--)
-	{
-		res=res * 10;
-	}
-	return res;
-}
+
 int main()
 {
-	int max = 0;
-	int prev = 0;
+	int amount = 0;
 	string blank = "";
-	ifstream text("C:\\Users\\user\\Downloads\\informatyka-2025-czerwiec-matura-rozszerzona-zalaczniki\\informatyka-2025-czerwiec-matura-rozszerzona-zalaczniki\\liczby2.txt");
-	while (getline(text, blank))
+	ifstream text("C:\\Users\\user\\Downloads\\informatyka-2025-czerwiec-matura-rozszerzona-zalaczniki\\informatyka-2025-czerwiec-matura-rozszerzona-zalaczniki\\dane.txt");
+	getline(text, blank);
+	auto isNumber = [](char character) {
+		return (character >= '0' && character <= '9');
+	};
+	for (int i = 0; i< blank.length() - 1; i++) 
 	{
-		int k = stoi(blank) * stoi(blank);
-		int n = stoi(blank);
-		int coun = 0;
-		int a = 0;
-		int b = 0;
-		int leng = 0;
-		int cop = k;
-		while (cop != 0)
-		{
-			cop = cop / 10;
-			leng++;
-		}
-		for (int i = 1; i < leng; i++)
-		{
-			b = k % ten(i);
-			a = (k / ten(i));
-			if (a + b <= n) 
-			{
-				coun++;
-			}
-			if (coun > max) 
-			{
-				prev = n;
-				max = coun;
-			}
+		char current = blank[i];
+		char next = blank[i + 1];
+		char previous = i == 0 ? ' ' : blank[i - 1];
+		if (current == '5' && next == '0' && !isNumber(previous)) {
+			amount++;
 		}
 	}
-	cout << prev << " " << max << endl;
+	cout << amount << endl;
 }
